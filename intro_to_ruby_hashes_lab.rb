4002bd6e6a4268fed6fb84bc65842548e5d71e22
Alice Freda <alicefreda@gmail.com>
2019-10-04 20:00:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
-        :name_four => "Zendaya"
+        :shortline_railroad => {}
       }
     }
   }
