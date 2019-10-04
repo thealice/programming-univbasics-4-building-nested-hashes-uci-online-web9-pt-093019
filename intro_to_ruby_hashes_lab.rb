21007d449be9ad1d8f84bd2c9cc8fc01b1f0725c
Alice Freda <alicefreda@gmail.com>
2019-10-04 19:47:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :one => "$100",
+        :name => "$100",
         :two => "$200",
         :three => "$300",
         :four => "$400"
