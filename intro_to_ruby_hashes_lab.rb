373c7e82ffb8491a72818f9e584c8f2ff155aab3
Alice Freda <alicefreda@gmail.com>
2019-10-04 20:09:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,7 +54,7 @@ def monopoly_with_fourth_tier
           "mortgage_value" => "$400"
         },
         :shortline_railroad => {
-          "mortgage_value" => "$450"
+          "mortgage_value" => "$800"
         }
       }
     }
